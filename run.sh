fc15d5b0daa7ecda309ee918ffe38f96d14f08e0
marcellagi <marcellagiovannass@gmail.com>
2022-11-03 15:13:22 -0300

    warthog navigation pack initialization containing move_base, AMCL, gmapping and exploration

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 set -e #exit on first error
 
-export HUSKY_TOP_PLATE_ENABLED="false"
-export HUSKY_IMU_XYZ="-0.122 0.0 0.7"
-export HUSKY_IMU_RPY="0.0 0.0  0.0"
+export WARTHOG_TOP_PLATE_ENABLED="false"
+export WARTHOG_IMU_XYZ="-0.122 0.0 0.7"
+export WARTHOG_IMU_RPY="0.0 0.0  0.0"
 
 roslaunch outdoor_waypoint_nav outdoor_waypoint_nav.launch
